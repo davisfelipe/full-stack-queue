d5edb97d51b4d9b40482de611066c7d65640bf3c
David Peña <davidpena@habi.co>
2023-04-05 21:17:36 -0500

    Feature: Update middlewares

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
@@ -11,6 +12,11 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
+
+type DefaultErrorHandler struct {
+	Message string `json:"message"`
+	Status  bool   `json:"status"`
+}
 
 func main() {
 	err := godotenv.Load(".env")
@@ -31,6 +37,31 @@ func main() {
 	s.Start(BindRoutes)
 }
 
+func CustomeMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.Method + " " + r.RequestURI)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func CustomNotFoundError(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
+		log.Println(r.Method + " " + r.RequestURI)
+		json.NewEncoder(w).Encode(DefaultErrorHandler{
+			Message: "Route not found",
+			Status:  false,
+		})
+	})
+}
+
 func BindRoutes(s server.Server, r *mux.Router) {
+
+	// Middleware
+	r.Use(CustomeMiddleware)
+	r.NotFoundHandler = CustomNotFoundError(r)
+
+	// Routes
 	r.HandleFunc("/", handlers.TestHandler(s)).Methods(http.MethodGet)
 }
---
+++ makefile
@@ -0,0 +1,7 @@
+
+run:
+	echo "running project"
+	go run main.go
+
+build:
+	go build main.go
